baffca4666edc6528ce37525761aa634e292c810
Johnny Hausman <johnny@appdevdesigns.net>
2014-07-03 12:24:11 +0700

    + noConflict() option to manage existing jQuery instances on a page:

--- assets/appdev/appdev.js
+++ assets/appdev/appdev.js
@@ -108,6 +108,59 @@ if (typeof window.AD == 'undefined') {
 
     }
 
+    
+    function versionCompare(v1, v2, options) {
+        var lexicographical = options && options.lexicographical,
+            zeroExtend = options && options.zeroExtend,
+            v1parts = v1.split('.'),
+            v2parts = v2.split('.');
+
+        function isValidPart(x) {
+            return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+        }
+
+        if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+            return NaN;
+        }
+
+        if (zeroExtend) {
+            while (v1parts.length < v2parts.length) v1parts.push("0");
+            while (v2parts.length < v1parts.length) v2parts.push("0");
+        }
+
+        if (!lexicographical) {
+            v1parts = v1parts.map(Number);
+            v2parts = v2parts.map(Number);
+        }
+
+        for (var i = 0; i < v1parts.length; ++i) {
+            if (v2parts.length == i) {
+                return 1;
+            }
+
+            if (v1parts[i] == v2parts[i]) {
+                continue;
+            }
+            else if (v1parts[i] > v2parts[i]) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+
+        if (v1parts.length != v2parts.length) {
+            return -1;
+        }
+
+        return 0;
+    }
+    AD.util = {};
+    AD.util.versionCompare = versionCompare;
+
+
+    // is it possible we have a jQuery version conflict?
+    var resolveConflict = false;
 
     steal(
 
@@ -118,9 +171,49 @@ if (typeof window.AD == 'undefined') {
                         [ 
                             { 
                                 // return true if loaded
-                                cond:function() { return ('undefined' != typeof window.jQuery ); },
+                                cond:function() { 
+
+                                    if ('undefined' != typeof window.jQuery ) {
+
+                                        // jQuery is loaded already, so do a version check
+                                        // if current version < our minimum (1.9.0) then
+                                        if (AD.util.versionCompare($.fn.jquery, '1.9.0', {lexicographical: true}) < 0) {
+
+                                            // this version is too old
+                                            // we're gonna load another jQuery
+                                            // and try not to Conflict with the existing one:
+                                            resolveConflict = true;  
+                                            return false;
+                                        } else {
+
+                                            // this version is good enough keep it.
+                                            return true;
+                                        }
+                                    }
+
+                                },
                                 lib:'js/jquery.min.js' 
                             } 
+                        ],
+
+                        [
+                            {
+                                // make sure this one is always added:
+                                cond:function() {
+                                    return false;
+                                }, 
+
+                                lib:function() {
+
+                                    AD.ui.jQuery = $;
+
+                                    if (resolveConflict) {
+                                        console.log(' .... trying to not conflict with existing jQuery');
+                                        $.noConflict();  // return things as they were.
+                                    }
+                                }
+                            }
+
                         ],
 
                         // and CanJS
@@ -136,28 +229,42 @@ if (typeof window.AD == 'undefined') {
 
     ).then(
 
-            'appdev/comm/hub.js',
-            'appdev/util/uuid.js',
-			'appdev/config/config.js'
-    )
-//    .then(
-//            'canjs/can.jquery.js'//,
-////			'appdev/config/data.js'
-//    )
-    .then(
-            'appdev/model/model.js',
-            'appdev/labels/lang.js',
-            'appdev/labels/label.js',
-            'appdev/comm/service.js',
-			'appdev/auth/reauth.js'
-    )
-    .then(
-            'appdev/UIController.js',
-            function($) {
+        function() {
 
-console.log('AD setup done ...');
+            // now because of the potential conflicts, we're going to make sure 
+            // all this is run with context of AD.ui.jQuery == $
 
-            }
+            (function($) {
+
+                steal(
+
+                        'appdev/comm/hub.js',
+                        'appdev/util/uuid.js',
+            			'appdev/config/config.js'
+                )
+            //    .then(
+            //            'canjs/can.jquery.js'//,
+            ////			'appdev/config/data.js'
+            //    )
+                .then(
+                        'appdev/model/model.js',
+                        'appdev/labels/lang.js',
+                        'appdev/labels/label.js',
+                        'appdev/comm/service.js',
+            			'appdev/auth/reauth.js'
+                )
+                .then(
+                        'appdev/UIController.js',
+                        function($) {
+
+            console.log('AD setup done ...');
+
+                        }
+                );
+
+            })(AD.ui.jQuery);
+
+        }
     );
 
 
