f8eb81b3e1a2bed9d4daf5ce871f43417eccb75d
Johnny Hausman <johnny@hausmanfamily.com>
2016-03-21 13:36:03 +0700

    +add: AD.error.log() updated to use webix.message() if available.

--- assets/appdev/error/log.js
+++ assets/appdev/error/log.js
@@ -34,7 +34,16 @@ steal(
 
     AD.error.log = function( message, data ) {
 
+        //// TODO: would be great to have some site config settings for 
+        ////    - useWebix: {bool}    true: dump message to webix.message();
+        ////    - webixTimeout: {int} expire value in ms
+        ////    - consoleLog: {bool}  true: dump message and data to console.error
         console.error(message, data);
+
+        // if the webix library is included, then post the message there.
+        if (webix) {
+            webix.message({ type:'error', text:message, expire:2000 });
+        }
 
         //// TODO: AD.comm.service.post({ url: 'url',  params: {message:message, data:data}});
     }
