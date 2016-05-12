b91c3c44cd218515e96e59648d9c90437bb22982
Johnny Hausman <johnny@hausmanfamily.com>
2016-05-12 14:59:06 +0700

    +add: re-enabled the ability to default to a specific user (in a non production environment)

--- api/services/ADCore.js
+++ api/services/ADCore.js
@@ -42,7 +42,8 @@ var passportSession = passport.session();
 
 
 // import local files
-var path = require('path');
+var path  = require('path');
+var Comm  = require(path.join(__dirname, 'adcore', 'comm.js'));
 var Queue = require(path.join(__dirname, 'adcore', 'queue.js'));
 
 
@@ -167,89 +168,29 @@ module.exports = {
             }
         },
 
-    },
 
+        testUser: function(req, guid) {
 
-    comm:{
+            // don't allow this in a production site!
+            if ((sails.config.environment.toLowerCase() != 'production')) {
 
-        error:function(res, err, code) {
+                var user = new User({ guid: guid }, { guid: guid });
 
-            var packet = {
-                status:'error',
-                data:err
-            };
+                req.user = user;
 
-            // add in optional properties: id, message
-            if (err.id) packet.id = err.id;
-            if (err.message) packet.message = err.message;
+                return user.ready();
 
-            // default to HTTP status code: 400
-            if ('undefined' == typeof code) code = 400; 
-
-
-            // Sails v0.11 no longer has res.header on socket connections
-            if(res.header) res.header('Content-type', 'application/json'); 
-
-            res.send(JSON.stringify(packet).replace('"false"', 'false').replace('"true"', 'true'), code);
-        },
-
-
-
-        reauth:function(res) {
-
-            var packet = {
-                id:5,
-                message:'Reauthenticate.',
-                authType: sails.config.appdev.authType
-            };
-
-            // add in additional auth info depending on
-            // authType
-            // packet.data[authType]
-
-            packet.data = {};
-
-            if ('local' == sails.config.appdev.authType) {
-                
-                packet.data[sails.config.appdev.authType] = {
-                        message:"submit username=[username]&password=[password] to this uri",
-                        method: 'post',
-                        uri:sails.config.appdev.authURI
-                }
+            } else {
+                var dfd = AD.sal.Defered();
+                dfd.reject( new Error('testUser is not allowed in production site.'));
+                return dfd;
             }
-            else {
-                // include CAS: { uri:'cas/url/here' }
-                packet.data[sails.config.appdev.authType] = {
-                        message:"1st authenticate with CAS.uri, then call our CAS.authUri:",
-                        uri:sails.config.cas.baseURL,
-                        authUri: sails.config.appdev.authURI
-                }
-            }
-
-            // NOTE: this == ADCore.comm
-            this.error(res, packet, 401);
-        },
-
-
-
-        success:function(res, data, code) {
-
-            var packet = {
-                status:'success',
-                data:data
-            };
-
-            // default to HTTP status code: 200
-            if ('undefined' == typeof code) code = 200; //AD.Const.HTTP.OK;  // 200: assume all is ok
-
-            // Sails v0.11 no longer has res.header on socket connections
-            if(res.header) res.header('Content-type', 'application/json');
-            
-            res.send(JSON.stringify(packet).replace('"false"', 'false').replace('"true"', 'true'), code);
         }
 
     },
 
+
+    comm: Comm,
 
 
     error: {
---
+++ api/services/adcore/comm.js
@@ -0,0 +1,106 @@
+/**
+ * @class  ADCore.comm
+ * @parent ADCore
+ * 
+ * The .comm object provides a common format for our responses to the OpsPortal.
+ *
+ * .error() 
+ * handles providing common error message formats.
+ *
+ * .reauth()
+ * provides a reauthenticate request for our client OpsPortal
+ *
+ * .success()
+ * provides the requested data back to the OpsPortal.
+ * 
+ * 
+ * ## Usage
+ *
+ */
+
+
+
+var AD = require('ad-utils');
+var _ = require('lodash');
+
+
+module.exports = {
+
+    error:function(res, err, code) {
+
+        var packet = {
+            status:'error',
+            data:err
+        };
+
+        // add in optional properties: id, message
+        if (err.id) packet.id = err.id;
+        if (err.message) packet.message = err.message;
+
+        // default to HTTP status code: 400
+        if ('undefined' == typeof code) code = 400; 
+
+
+        // Sails v0.11 no longer has res.header on socket connections
+        if(res.header) res.header('Content-type', 'application/json'); 
+
+        res.send(JSON.stringify(packet).replace('"false"', 'false').replace('"true"', 'true'), code);
+    },
+
+
+
+    reauth:function(res) {
+
+        var packet = {
+            id:5,
+            message:'Reauthenticate.',
+            authType: sails.config.appdev.authType
+        };
+
+        // add in additional auth info depending on
+        // authType
+        // packet.data[authType]
+
+        packet.data = {};
+
+        if ('local' == sails.config.appdev.authType) {
+            
+            packet.data[sails.config.appdev.authType] = {
+                    message:"submit username=[username]&password=[password] to this uri",
+                    method: 'post',
+                    uri:sails.config.appdev.authURI
+            }
+        }
+        else {
+            // include CAS: { uri:'cas/url/here' }
+            packet.data[sails.config.appdev.authType] = {
+                    message:"1st authenticate with CAS.uri, then call our CAS.authUri:",
+                    uri:sails.config.cas.baseURL,
+                    authUri: sails.config.appdev.authURI
+            }
+        }
+
+        // NOTE: this == ADCore.comm
+        this.error(res, packet, 401);
+    },
+
+
+
+    success:function(res, data, code) {
+
+        var packet = {
+            status:'success',
+            data:data
+        };
+
+        // default to HTTP status code: 200
+        if ('undefined' == typeof code) code = 200; //AD.Const.HTTP.OK;  // 200: assume all is ok
+
+        // Sails v0.11 no longer has res.header on socket connections
+        if(res.header) res.header('Content-type', 'application/json');
+        
+        res.send(JSON.stringify(packet).replace('"false"', 'false').replace('"true"', 'true'), code);
+    }
+
+
+};
