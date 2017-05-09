58b4ca2395eaca3c7555f966ad7b9c242dde15dd
Joshua Chan <joshua@appdevdesigns.net>
2017-05-08 22:47:38 -0700

    * Steal.js uses the client side appdev/config/data.js file when it's been compiled into the dist bundle in production mode. Make it fetch live config from the server.

--- assets/appdev/config/data.js
+++ assets/appdev/config/data.js
@@ -1,17 +1,61 @@
 // Under normal circumstances, this file should never get loaded. Instead,
 // the server will run appdev-core/ADCoreController.js configData().
+//
+// However, this file might be invoked if steal.js compiles everything and
+// bypasses the server route for appdev/config/data.js . 
 
 steal(
     'appdev/config/config.js',
+    'jquery',
     function () {
+        
+        // Set the default config if we can't fetch from the server
+        var fallback = function() {
+            AD.config.setValue('authType', 'CAS');
+        };
+        
+        $.ajax({ url: '/appdev/config/data.json' })
+        .done(function(response) {
+            if (!response.data) fallback();
+            else {
+                var settings = response.data;
+                for (var key in settings) {
+                    AD.config.setValue(key, settings[key]);
+                }
+                if (settings.siteBaseURL) {
+                    
+                    // socket.io base URL
+                    window.io = window.io || {};
+                    window.io.sails = window.io.sails || {};
+                    window.io.sails.url = settings.siteBaseURL;
+                    
+                    // Steal.js base URL
+                    if (typeof System != 'undefined') {
+                        System.baseURL = settings.siteBaseURL;
+                    }
+                    
+                    // CanJS ajax base URL
+                    if (typeof can != 'undefined' && can.ajax) {
+                        var original = can.ajax;
+                        can.ajax = function(a, b) {
+                            if (typeof a == 'string' && a[0] == '/') {
+                                a = settings.siteBaseURL + a;
+                            }
+                            else if (typeof a == 'object' && a.url && a.url[0] == '/') {
+                                a.url = settings.siteBaseURL + a.url;
+                            }
+                            return original(a, b);
+                        };
+                    }
+                    
+                    console.log('Base URL applied to canJS');
+                }
+            }
+        })
+        .fail(function(err) {
+            console.error(err);
+            fallback();
+        });
 
-        var configValues = {
-            authType: 'CAS'
-        }
-
-
-        for (var v in configValues) {
-            AD.config.setValue(v, configValues[v]);
-        }
-
-    });
+    }
+);
