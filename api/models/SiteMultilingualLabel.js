847dbebb6ef891a335f3d53e5a7355de4ee76da3
Joshua Chan <joshua@appdevdesigns.net>
2022-03-28 01:08:45 -0700

    Convenience method for fetching all labels from a given context

--- api/models/SiteMultilingualLabel.js
+++ api/models/SiteMultilingualLabel.js
@@ -44,8 +44,60 @@ module.exports = {
             maxLength: 80
         },
 
+    },
 
+    ////////////////////////////
+    // Model class methods
+    ////////////////////////////
+
+
+    /**
+     * Fetch all labels from a given context and language, returned in the 
+     * form of a dictionary (associative array).
+     * 
+     * @param {string} context
+     * @param {string} [languageCode]
+     *      Default is sails.config.appdev['lang.default']
+     * @return {Promise}
+     *      Resolves with object (dictionary)
+     *      {
+     *          <label_key>: <label_label>,
+     *          <label_key>: <label_label>,
+     *          ...
+     *      }
+     */
+    getLabels: function(context, languageCode) {
+        languageCode = languageCode || sails.config.appdev["lang.default"];
+        
+        return new Promise((resolve, reject) => {
+            .then(() => {
+                SiteMultilingualLabel.query(
+                    `
+                        SELECT
+                            label_key AS key,
+                            label_label AS label
+                        FROM
+                            site_multilingual_label
+                        WHERE
+                            label_context = ?
+                            AND language_code = ?
+                    `,
+                    [context, languageCode],
+                    (err, list) => {
+                        if (err) reject(err);
+                        else {
+                            var result = {};
+                            list = list || [];
+                            list.forEach((row) => {
+                                result[row.key] = row.label;
+                            });
+                            resolve(result);
+                        }
+                    }
+                );
+            });
     }
+
 };
 
 
